730ec1e177d1d2fb970651a0a31897a50d3a98ca
arimulian <arimuliansyahmanurung@gmail.com>
2025-05-16 17:31:26 +0700

    fix(Error Handling): NotFoundHTTPException

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,5 +18,12 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'status' => 'false',
+                    'message' => 'Route Not found',
+                ], 404);
+            }
+        });
     })->create();
